7373e676bbe0a3e01a65bc7da25e63dc9e87b5c9
Jose Federico <federicj@gmatc.matc.edu>
2022-10-20 11:02:40 -0500

    Completed activity 6

--- blog/src/pages/index.js
+++ blog/src/pages/index.js
@@ -4,56 +4,59 @@ import { GatsbyImage } from  "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import { List, ListItem } from '../components/List'
 import * as styles from "../components/index.module.css"
 
-const IndexPage = ({data})=>(
+const IndexPage = ({ data }) => (
   <Layout>
-    <Seo title="Home"/>
-      <ul className={styles.list}>
-        {
-          data.allContentfulBlogPost.edges.map(edge =>(
-          
-            <li key={edge.node.id}>
-              <Link to={edge.node.slug}>{edge.node.title}</Link>
-              <div>
-                <GatsbyImage 
-                  image={edge.node.heroImage.gatsbyImageData}
-                />
-              </div>
-              <div>
-                {edge.node.body.childMarkdownRemark.excerpt}
-              </div>
-            </li>
-          ))
-        }
-      </ul>
+    <Seo title="Home" />
+    <List width={[1, 2/3, 7/8]} p={2}>
+      {
+        data.allContentfulBlogPost.edges.map(edge => (
+          <ListItem p={3} key={edge.node.id}>
+            <Link to={edge.node.slug}>{edge.node.title}</Link>
+            <div>
+            <GatsbyImage 
+              image={edge.node.heroImage.gatsbyImageData}
+            />
+            </div>
+            <div>
+              {edge.node.body.childMarkdownRemark.excerpt}
+            </div>
+          </ListItem>
+        ))
+      
+      }
+    </List>
   </Layout>
 )
+
+export const Head = () => <Seo title="Home" />
 
 export default IndexPage
 
 export const query = graphql`
-{
-  allContentfulBlogPost {
-    edges {
-      node {
-        id
-        title
-        slug
-        body {
-          childMarkdownRemark {
-          excerpt 
+  {
+    allContentfulBlogPost {
+      edges {
+        node {
+          id
+          title
+          slug
+          body {
+            childMarkdownRemark {
+              excerpt
+            }
           }
-        }
-        heroImage {
-          gatsbyImageData (
-            layout:CONSTRAINED
-            placeholder:BLURRED
-            width: 300
-          )
+          heroImage {
+            gatsbyImageData(
+              layout: CONSTRAINED
+              placeholder: BLURRED
+              width: 600
+            )
+          }
         }
       }
     }
   }
-}
-`
+  `
