380df350409152834498f8624d709641f806e894
Jose Federico <federicj@gmatc.matc.edu>
2022-11-09 17:06:20 -0600

    Activity 9 complete / heroImage working

--- blog/src/pages/index.js
+++ blog/src/pages/index.js
@@ -1,35 +1,50 @@
 import * as React from "react"
 import { graphql, Link } from "gatsby"
-import {Box, Card, Image, Heading} from 'rebass'
-import styled from "styled-components"
+import { GatsbyImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import styled from "styled-components"
+import { Box, Card } from "rebass"
 
 const Grid = styled(Box)`
-box-sizing: border-box;
-margin: 0px;
-min-width: 0px;
-display: grid;
-gap: 100px;
-grid-template-columns: repeat(auto-fit, minmax(128px, 1fr));
+  box-sizing: border-box;
+  margin: 0;
+  min-width: 0px;
+  display: grid;
+  row-gap: 30px;
+  align-self: center;
+  justify-self: center;
+  grid-template-columns: repeat(auto-fit, minmax(256px, 1fr));
 `
 
-const IndexPage = ({data})=>(
+const IndexPage = ({ data }) => (
   <Layout>
-  <Seo title="Home"/>
-<Grid>
-{
-  data.allContentfulBlogPost.edges.map(edge=>(
-    <Card key= {edge.node.id} width= {256} p={3}>
-    <Link to={edge.node.slug}>
-      <Image src ={edge.node.heroImage.url} alt="hero image"/>
-    </Link>
-    <Heading>{edge.node.title}</Heading>
-    <div>{edge.node.body.childMarkdownRemark.excerpt}</div>
-    </Card>
-  ))
-}
-</Grid>
+    <Seo title="Home" />
+
+    <Grid>
+      {data.allContentfulBlogPost.edges.map(edge => (
+        <Card
+          key={edge.node.id}
+          width={256}
+          p={3}
+          sx={{
+            borderWidth: "1px",
+            borderStyle: "solid",
+            borderColor: "grey",
+            justifySelf: "center",
+          }}
+        >
+          <Link to={edge.node.slug}>{edge.node.title}</Link>
+          <div>
+            <GatsbyImage
+              image={edge.node.heroImage.gatsbyImageData}
+              alt={edge.node.title}
+            />
+          </div>
+          <div>{edge.node.body.childMarkdownRemark.excerpt}</div>
+        </Card>
+      ))}
+    </Grid>
   </Layout>
 )
 
@@ -61,4 +76,4 @@ export const query = graphql`
       }
     }
   }
-  `
+`
